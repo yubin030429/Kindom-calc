4dd6abfba1ad360ab316d0fc045cca5580dc18a0
badang <68318039+yubin030429@users.noreply.github.com>
2022-06-14 17:28:08 +0900

    add cookierun font-face

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app>
-    <div>testing</div>
+  <v-app id="default">
+    <div class="test">testinasdfg</div>
   </v-app>
 </template>
 
@@ -11,3 +11,22 @@ export default {
   }),
 };
 </script>
+
+<style>
+#default {
+  font-family: "CookieRunRegular", sans-serif !important;
+}
+
+@font-face {
+  font-family: "CookieRunBlack";
+  src: url("@/assets/fonts/CookieRunBlack.ttf");
+}
+@font-face {
+  font-family: "CookieRunBold";
+  src: url("@/assets/fonts/CookieRunBold.ttf");
+}
+@font-face {
+  font-family: "CookieRunRegular";
+  src: url("@/assets/fonts/CookieRunRegular.ttf");
+}
+</style>
