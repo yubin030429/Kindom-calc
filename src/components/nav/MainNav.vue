d23d25a3e9ebef9307613499a4f2c7d5a54bdeba
badang <68318039+yubin030429@users.noreply.github.com>
2022-06-14 21:04:14 +0900

    make left nav contents

--- src/components/nav/MainNav.vue
+++ src/components/nav/MainNav.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>asdf</div>
+  <v-card style="top: 1vh" flat height="93vh" width="240">
+    <v-navigation-drawer permanent>
+      <NavContent />
+    </v-navigation-drawer>
+  </v-card>
 </template>
+
+<script>
+import NavContent from "@/components/nav/NavContent.vue";
+
+export default {
+  components: {
+    NavContent,
+  },
+};
+</script>
