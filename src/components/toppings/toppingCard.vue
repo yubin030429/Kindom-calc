11bc8d29179d10032aed3821d46a48e50a8702b3
badang <68318039+yubin030429@users.noreply.github.com>
2022-06-16 15:42:47 +0900

    make toppingOpt props

---
+++ src/components/toppings/toppingCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <v-card>
+      <v-card-title> {{ $props.toppingOpt.topping }} </v-card-title>
+
+      <v-card-content> {{ $props.toppingOpt.subCool }} </v-card-content>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["toppingOpt"],
+};
+</script>
--- src/views/toppingSet.vue
+++ src/views/toppingSet.vue
@@ -1,3 +1,25 @@
 <template>
-  <v-app> </v-app>
+  <v-app>
+    <v-row>
+      <v-col v-for="(item, i) in items" :key="item">
+        <toppingCard :toppingOpt="items[i]" />
+      </v-col>
+    </v-row>
+  </v-app>
 </template>
+
+<script>
+import toppingCard from "@/components/toppings/toppingCard.vue";
+
+export default {
+  components: {
+    toppingCard,
+  },
+
+  data() {
+    return {
+      items: [{ topping: "raspberry", subCool: "1.2" }],
+    };
+  },
+};
+</script>
