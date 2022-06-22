a15814102d7e12eb9804cb8c3b05ae2515f50804
badang <68318039+yubin030429@users.noreply.github.com>
2022-06-22 14:29:39 +0900

    make EditBtn component

---
+++ src/components/EditBtn.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-speed-dial
+    v-model="fab"
+    bottom
+    right
+    direction="left"
+    transition="slide-x-reverse-transition"
+  >
+    <template v-slot:activator>
+      <v-btn v-model="fab" color="orange lighten-2" dark fab>
+        <v-icon v-if="fab"> mdi-close </v-icon>
+        <v-icon v-else> mdi-account-circle </v-icon>
+      </v-btn>
+    </template>
+    <v-btn fab dark small color="green">
+      <v-icon>mdi-pencil</v-icon>
+    </v-btn>
+    <v-btn fab dark small color="indigo">
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+    <v-btn fab dark small color="red">
+      <v-icon>mdi-delete</v-icon>
+    </v-btn>
+  </v-speed-dial>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+};
+</script>
--- src/views/toppingSet.vue
+++ src/views/toppingSet.vue
@@ -1,25 +1,35 @@
 <template>
-  <v-app>
-    <v-row>
-      <v-col v-for="(item, i) in items" :key="item">
-        <toppingCard :toppingOpt="items[i]" />
-      </v-col>
-    </v-row>
-  </v-app>
+  <div>
+    <v-app>
+      <v-row>
+        <v-col v-for="(item, i) in items" :key="item">
+          <toppingCard :toppingOpt="items[i]" />
+        </v-col>
+      </v-row>
+      <EditBtn />
+    </v-app>
+  </div>
 </template>
 
 <script>
 import toppingCard from "@/components/toppings/toppingCard.vue";
+import EditBtn from "@/components/EditBtn.vue";
 
 export default {
   components: {
     toppingCard,
+    EditBtn,
   },
 
   data() {
     return {
-      items: [{ topping: "raspberry", subCool: "1.2" }],
+      items: [
+        { topping: "raspberry", subCool: "1.2" },
+        { topping: "raspberry", subCool: "1.2" },
+      ],
     };
   },
 };
 </script>
+
+<style></style>
